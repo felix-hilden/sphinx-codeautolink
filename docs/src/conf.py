76ea0c084cb71dc91585d3a781267a467c7fd83a
Felix Hildén <felix.hilden@gmail.com>
2025-01-08 09:15:50 +0200

    Happy new year!

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, str(_src_dir))
 
 project = "sphinx-codeautolink"
 author = "Felix Hildén"
-copyright = "2021-2023, Felix Hildén"
+copyright = "2021-2025, Felix Hildén"
 version = __version__
 release = version
 
