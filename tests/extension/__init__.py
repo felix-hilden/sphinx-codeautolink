750ada87bc18b824b2d5be35fba7c40e1622e409
Jan Lebert <jan.lebert@ucsf.edu>
2025-03-05 17:27:56 -0800

    Partial revert beafd53

--- tests/extension/__init__.py
+++ tests/extension/__init__.py
@@ -32,7 +32,6 @@ codeautolink_warn_on_no_backreference = False
 any_whitespace = re.compile(r"\s*")
 ref_tests = [(p.name, p) for p in Path(__file__).with_name("ref").glob("*.txt")]
 ref_xfails = {}
-builders = ["html", "dirhtml"]
 
 
 def assert_links(file: Path, links: list):
@@ -47,8 +46,7 @@ def assert_links(file: Path, links: list):
 
 
 @pytest.mark.parametrize(("name", "file"), ref_tests)
-@pytest.mark.parametrize("builder", builders)
-def test_references(name: str, file: Path, builder: str, tmp_path: Path):
+def test_references(name: str, file: Path, tmp_path: Path):
     """
     Basic extension tests for reference building.
 
@@ -73,7 +71,7 @@ def test_references(name: str, file: Path, builder: str, tmp_path: Path):
 
     files = {"conf.py": default_conf + conf, "index.rst": index}
     print(f"Building file {name}.")
-    result_dir = _sphinx_build(tmp_path, builder, files)
+    result_dir = _sphinx_build(tmp_path, "html", files)
 
     assert_links(result_dir / "index.html", links)
     assert check_link_targets(result_dir) == len(links)
@@ -83,8 +81,7 @@ table_tests = list(Path(__file__).with_name("table").glob("*.txt"))
 
 
 @pytest.mark.parametrize("file", table_tests)
-@pytest.mark.parametrize("builder", builders)
-def test_tables(file: Path, builder: str, tmp_path: Path):
+def test_tables(file: Path, tmp_path: Path):
     """
     Tests for backreference tables.
 
@@ -110,7 +107,7 @@ def test_tables(file: Path, builder: str, tmp_path: Path):
         links = []
 
     files = {"conf.py": default_conf + conf, "index.rst": index}
-    result_dir = _sphinx_build(tmp_path, builder, files)
+    result_dir = _sphinx_build(tmp_path, "html", files)
 
     index_html = result_dir / "index.html"
     text = index_html.read_text("utf-8")
@@ -127,8 +124,7 @@ fail_tests = list(Path(__file__).with_name("fail").glob("*.txt"))
 
 
 @pytest.mark.parametrize("file", fail_tests)
-@pytest.mark.parametrize("builder", builders)
-def test_fails(file: Path, builder: str, tmp_path: Path):
+def test_fails(file: Path, tmp_path: Path):
     """
     Tests for failing builds.
 
@@ -142,7 +138,7 @@ def test_fails(file: Path, builder: str, tmp_path: Path):
     conf, index = file.read_text("utf-8").split("# split")
     files = {"conf.py": default_conf + conf, "index.rst": index}
     with pytest.raises(RuntimeError):
-        _sphinx_build(tmp_path, builder, files)
+        _sphinx_build(tmp_path, "html", files)
 
 
 def test_non_html_build(tmp_path: Path):
@@ -162,8 +158,7 @@ Test project
     _sphinx_build(tmp_path, "man", files)
 
 
-@pytest.mark.parametrize("builder", builders)
-def test_build_twice_and_modify_one_file(builder: str, tmp_path: Path):
+def test_build_twice_and_modify_one_file(tmp_path: Path):
     index = """
 Test project
 ------------
@@ -192,12 +187,11 @@ But edited.
 .. autolink-examples:: test_package.bar
 """
     files = {"conf.py": default_conf, "index.rst": index, "another.rst": another}
-    _sphinx_build(tmp_path, builder, files)
-    _sphinx_build(tmp_path, builder, {"another.rst": another2})
+    _sphinx_build(tmp_path, "html", files)
+    _sphinx_build(tmp_path, "html", {"another.rst": another2})
 
 
-@pytest.mark.parametrize("builder", builders)
-def test_build_twice_and_delete_one_file(builder: str, tmp_path: Path):
+def test_build_twice_and_delete_one_file(tmp_path: Path):
     conf = default_conf + "\nsuppress_warnings = ['toc.not_readable']"
     index = """
 Test project
@@ -221,13 +215,12 @@ Another
 """
 
     files = {"conf.py": conf, "index.rst": index, "another.rst": another}
-    _sphinx_build(tmp_path, builder, files)
+    _sphinx_build(tmp_path, "html", files)
     (tmp_path / "src" / "another.rst").unlink()
-    _sphinx_build(tmp_path, builder, {})
+    _sphinx_build(tmp_path, "html", {})
 
 
-@pytest.mark.parametrize("builder", builders)
-def test_raise_unexpected(builder: str, tmp_path: Path):
+def test_raise_unexpected(tmp_path: Path):
     index = """
 Test project
 ------------
@@ -250,14 +243,13 @@ Test project
 
     target = "sphinx_codeautolink.parse.ImportTrackerVisitor"
     with pytest.raises(RuntimeError), patch(target, raise_msg):
-        _sphinx_build(tmp_path, builder, files)
+        _sphinx_build(tmp_path, "html", files)
 
     with pytest.raises(RuntimeError), patch(target, raise_nomsg):
-        _sphinx_build(tmp_path, builder, files)
+        _sphinx_build(tmp_path, "html", files)
 
 
-@pytest.mark.parametrize("builder", builders)
-def test_parallel_build(builder: str, tmp_path: Path):
+def test_parallel_build(tmp_path: Path):
     index = """
 Test project
 ------------
@@ -282,19 +274,15 @@ Test project
     index = index + "\n   ".join(["", *list(subfiles)])
     files = {"conf.py": default_conf, "index.rst": index}
     files.update({k + ".rst": v for k, v in subfiles.items()})
-    result_dir = _sphinx_build(tmp_path, builder, files, n_processes=4)
+    result_dir = _sphinx_build(tmp_path, "html", files, n_processes=4)
 
     for file in subfiles:
-        if builder == "dirhtml" and file != "index":
-            assert_links(result_dir / file / "index.html", links)
-        else:
-            assert_links(result_dir / (file + ".html"), links)
+        assert_links(result_dir / (file + ".html"), links)
 
     assert check_link_targets(result_dir) == n_subfiles * len(links)
 
 
-@pytest.mark.parametrize("builder", builders)
-def test_skip_identical_code(builder: str, tmp_path: Path):
+def test_skip_identical_code(tmp_path: Path):
     """Code skipped and then linked in an identical block after."""
     index = """
 Test project
@@ -314,7 +302,7 @@ Test project
 .. automodule:: test_project
 """
     files = {"conf.py": default_conf, "index.rst": index}
-    result_dir = _sphinx_build(tmp_path, builder, files)
+    result_dir = _sphinx_build(tmp_path, "html", files)
 
     index_html = result_dir / "index.html"
     text = index_html.read_text("utf-8")
